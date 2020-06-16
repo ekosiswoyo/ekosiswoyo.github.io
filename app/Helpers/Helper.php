b0108533183831819905e3cf7e0c9f5191da31b6
Eko Siswoyo <ekoputra351@gmail.com>
2020-06-16 15:13:50 +0700

    revisi

--- app/Helpers/Helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function set_active( $route ) {
-    if( is_array( $route ) ){
-        return in_array(Request::path(), $route) ? 'active' : '';
-    }
-    return Request::path() == $route ? 'active' : '';
-}
